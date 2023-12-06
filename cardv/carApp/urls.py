ecbf4007ed0c73e359ba1627f89dc9e98c0eff7e
ipiwixx <antoine13141@gmail.com>
2023-12-06 16:10:05 +0100

    last commit

--- cardv/carApp/urls.py
+++ cardv/carApp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('voitures/', views.voiture_list, name="voitures")
+    path('voitures/', views.voiture_list, name="voitures"),
+    path('voiture/<int:voiture_id>/', views.voiture_detail, name="voiture")
+
 ]
--- cardv/carApp/views.py
+++ cardv/carApp/views.py
@@ -4,4 +4,8 @@ from .models import Voiture
 def voiture_list(request):
     voitures = Voiture.objects.all()
     return render(request, 'car/voiture_list.html', {'voitures':voitures})
+
+def voiture_detail(request, voiture_id):
+    voiture = Voiture.objects.get(id=voiture_id)
+    return render(request, 'car/voiture_detail.html', {'voiture':voiture})
 # Create your views here.
